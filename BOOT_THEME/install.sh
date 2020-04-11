466e0e0caa93131eaaa03cf97f77d55ce6b19f1b
Tom_The_Geek <tgbryan39@gmail.com>
2020-04-11 14:33:02 +0100

    Make boot theme installer chech for root access before running

--- BOOT_THEME/install.sh
+++ BOOT_THEME/install.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run this script as root!"
+  exit
+fi
+
 echo This script will install the mediapi BOOT SPLASH THEME to
 echo 		/usr/share/plymouth/themes
 echo and will also set this theme as the default
 echo
 
 echo Copying files
-sudo cp -r mediapi/ /usr/share/plymouth/themes/
+cp -r mediapi/ /usr/share/plymouth/themes/
 echo Done! Setting theme as default...
-sudo plymouth-set-default-theme -R mediapi
+plymouth-set-default-theme -R mediapi
 echo Installation complete!
